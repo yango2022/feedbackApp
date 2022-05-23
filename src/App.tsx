c8c6f0e1e57ab321de4dcb8c359ad868c8c5525d
Yango <louremido21@yahoo.com>
2022-05-23 14:50:55 +0100

    Fazendo o WidgetForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-interface LinkProps {
-  text: string;
-}
+import { Widget } from "./Components/Widget";
 
-function Button(props: LinkProps) {
-  return <button className="bg-sky-500 px-4 h-10 rounded-full">{props.text}</button>
-}
-
-function App() {
+export function App(){
   return (
-    <div className="flex gap-2">
-      <Button text="Upload" />
-      <Button text="Download" />
+    <div>
+      <Widget />
     </div>
-    
   );
 }
-
-export default App
